effe2e9ec84384d53368294991e714e618cb0740
Ezinne Nnamani <nnamani.ezinne@gmail.com>
2022-03-24 22:24:26 +0100

    Delete feature implemented
    Created the dlecte routes and the controller for the handling the end point. Also Created the button for the delete request

--- myapp/controllers/landing.js
+++ myapp/controllers/landing.js
@@ -71,3 +71,15 @@ exports.delete_lead = function(req, res, next) {
     }
   )
 }
+
+exports.delete_lead_json = function(req, res, next) {
+  return models.Lead.destroy({
+    where: {
+      id: req.params.lead_id
+    }
+  }). then(
+    result => {
+      res.send({ msg: "Sucess"})
+    }
+  )
+}
